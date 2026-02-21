b7baed7fae1dc04a0d359cabbb18f40ae8593f90
Skylar-Brown <skybrown26@gmail.com>
2026-02-20 21:32:45 -0500

    ProductTransformer: Introduce ProductTransformer interface to support polymorphic ETL transformations. DefaultProductTransformer: Implement DefaultProductTransformer with A2-equivalent rules and price range logic.

--- src/org/howard/edu/lsp/assignment3/ProductTransformer.java
+++ src/org/howard/edu/lsp/assignment3/ProductTransformer.java
@@ -1,1 +1,15 @@
 package org.howard.edu.lsp.assignment3;
+
+/**
+ * Strategy interface for transforming products.
+ */
+public interface ProductTransformer {
+  /**
+   * Transforms an input product into an output row for the transformed CSV.
+   *
+   * @param product input product
+   * @return a transformed CSV row with 5 fields:
+   *         ProductID, Name, Price, Category, PriceRange
+   */
+  String[] transform(Product product);
+}
